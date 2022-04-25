d72e3d7e014cd2ad36d5e147773f47605b44b50f
Jaine PS <jaine.aps@hotmail.com>
2022-04-25 17:39:12 -0300

    Desenho da pg "heroes".

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.css']
+
 })
 export class HeroeComponent implements OnInit {
 
