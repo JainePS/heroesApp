c72fcbbb709bf4a2662c65e63f64138de6edbf33
Jaine PS <jaine.aps@hotmail.com>
2022-04-26 02:07:25 -0300

    Informação de status das petições de registro.

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -1,47 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { HeroeModel } from 'src/app/models/heroe.model';
 import { HeroesService } from 'src/app/services/heroes.service';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-
 })
 export class HeroeComponent implements OnInit {
-
   heroe = new HeroeModel();
 
+  constructor(private heroesService: HeroesService) {}
 
-  constructor(private heroesService: HeroesService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  toHold(form: NgForm){
-
-    if(form.invalid){
+  toHold(form: NgForm) {
+    if (form.invalid) {
       console.log('Invalid form');
-      
-      
+
       return;
     }
 
-    if(this.heroe.id){
-      this.heroesService.actualizeRegister(this.heroe).
-      subscribe( result => {
-        console.log(result);
-        
-      })  
-    } else{
-        this.heroesService.createHeroe(this.heroe).
-        subscribe( result => {
-          console.log(result);
-          this.heroe = result;
-        })  
-      }
+    Swal.fire({
+      title: 'Wait',
+      text: 'Holding the information',
+      icon: 'info',
+      allowOutsideClick: false
+});
+ 
+    Swal.showLoading();
 
+    let petition:Observable<any>;
 
+    if (this.heroe.id) {
+      petition = this.heroesService.actualizeRegister(this.heroe);
+    } else {
+    petition = this.heroesService.createHeroe(this.heroe);
+    }
+
+    petition.subscribe( result => {
+      Swal.fire({
+        title: this.heroe.name,
+        text: 'Actualizated correctly',
+        icon: 'success'
+      })
+    })
   }
-
 }
